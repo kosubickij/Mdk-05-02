658df3d8146e5c3941f77362e2cd8381548325ca
kosubitskij <kosubickijdmitrij@gmail.com>
2026-09-17 17:13:45 +0300

    index.php

--- ПРАКТИЧЕСКАЯ НОМЕР 2/index.php
+++ ПРАКТИЧЕСКАЯ НОМЕР 2/index.php
@@ -12,18 +12,30 @@
     $c = 10;
     $d = 5;
     $rez = ($a/$c)*($b/$d)-($a*$b-$c)/($c*$d);
-    echo "Результат: $rez";
-    ?><br>
+    ?>
+    <?php echo "a = $a"; ?><br>
+    <?php echo "b = $b"; ?><br>
+    <?php echo "c = $c"; ?><br>
+    <?php echo "d = $d"; ?><br>
+    <?php echo "Результат: $rez"; ?><br>
     <?php 
     $x = 10;
     $y = 5;
+    ?>
+    <?php echo "x = $x"; ?><br>
+    <?php echo "y = $y"; ?><br>
+    <?php
     $rez = ($x + $y) / ($y+1) - ($x*$y - 12) / (34 + $x);
     echo "Результат: $rez";
     ?><br>
+    <?php echo "x = $x"; ?><br>
+    <?php echo "y = $y"; ?><br>
     <?php 
     $rez = (($x+1)/($x-1))**$x + (18 * $x * $y**2);
     echo "Результат: $rez";
     ?><br>
+    <?php echo "x = $x"; ?><br>
+    <?php echo "y = $y"; ?><br>
     <?php 
     $rez = (1 + 1/$x**2)**2 - (12*$x**2*$y);
     echo "Результат: $rez";
